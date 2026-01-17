ea8a80db7d475920613b492aafa3e3b264fc788b
Daniel Codena <dacodena1@espe.edu.ec>
2026-01-16 21:03:39 -0500

    Agregar menú principal para cálculos de física en main

--- MenuFisica/MenuFisica.cpp
+++ MenuFisica/MenuFisica.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include "Distancia.h"
+#include "Velocidad.h"
+#include "Tiempo.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int opcion;
+
+    do {
+        std::cout << "\n--- MENU DE FISICA ---\n";
+        std::cout << "1. Calcular distancia\n";
+        std::cout << "2. Calcular velocidad\n";
+        std::cout << "3. Calcular tiempo\n";
+        std::cout << "4. Salir\n";
+        std::cout << "Seleccione una opcion: ";
+        std::cin >> opcion;
+
+        switch (opcion) {
+        case 1: {
+            Distancia d;
+            d.calcular();
+            break;
+        }
+        case 2: {
+            Velocidad v;
+            v.calcular();
+            break;
+        }
+        case 3: {
+            Tiempo t;
+            t.calcular();
+            break;
+        }
+        case 4:
+            std::cout << "Saliendo del programa...\n";
+            break;
+        default:
+            std::cout << "Opcion invalida.\n";
+        }
+
+    } while (opcion != 4);
+
+    return 0;
 }
